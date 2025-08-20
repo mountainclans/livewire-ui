d269383ae0b55222eb51158902f1f2a61df86b2b
Vladimir Bajenov <vladimir@bajenov.info>
2025-08-20 16:13:22 +0400

    Toggle styles

--- resources/views/components/toggle.blade.php
+++ resources/views/components/toggle.blade.php
@@ -4,7 +4,7 @@
     'vertical' => false,
 ])
 
-<div>
+<div class="flex flex-col">
     <label @class([
         "inline-flex cursor-pointer",
         'flex-col items-start' => $vertical,
@@ -19,7 +19,7 @@
         <div class="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600 order-1"></div>
 
         <span @class([
-            'text-sm font-medium text-gray-900 dark:text-gray-300',
+            'text-sm font-medium text-gray-900 dark:text-white',
             'mb-1 order-0' => $vertical,
             'ms-3 order-2' => !$vertical,
         ])>{{ $label }}</span>
